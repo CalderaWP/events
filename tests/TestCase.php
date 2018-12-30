f8d0180ea7ff947b3ec307b98730d59679d925a5
Josh Pollock <josh@calderawp.com>
2018-12-30 11:24:18 -0500

    more work on entries to database wiring

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,13 +1,7 @@
 <?php
-
-
 namespace calderawp\caldera\Events\Tests;
 
-use calderawp\caldera\core\CalderaCore;
-use calderawp\caldera\Events\CalderaEvents;
-use calderawp\caldera\restApi\CalderaRestApi;
 use calderawp\caldera\Core\Tests\Traits\SharedFactories;
-use calderawp\CalderaContainers\Service\Container;
 
 abstract class TestCase extends \Mockery\Adapter\Phpunit\MockeryTestCase
 {
